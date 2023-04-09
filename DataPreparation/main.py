8785c0e4eb5a8c370967ed159f1165141cfc0204
Christopher Liu <chrisckeyliu@gmail.com>
2023-04-09 21:21:08 +0800

    Solve one issue

--- DataPreparation/main.py
+++ DataPreparation/main.py
@@ -1,11 +1,10 @@
 from downloader import download
-from video2flo import video2flo
-import os
+from video2flo import video2flo, check_path
 
 
 url = "https://www.bilibili.com/video/BV1et411t7Cm"
 Path = "./download"
-files0 = os.listdir(Path)
+check_path(Path)
 title = download(url, False, 10, path=Path)
 print("New file: ", title)
 video2flo(url, 0)
